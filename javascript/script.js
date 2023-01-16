1e31eb7043e09ac86b4d1f9eaa740cc7f19a3de2
Sarah A <sarahmoore26@gmail.com>
2023-01-16 19:27:42 +0000

    Testing commit

---
+++ javascript/script.js
@@ -0,0 +1,41 @@
+// // place holder JS for reference later
+// const matrix = {
+//     rock: {
+//       rock: 'draw',
+//       paper: 'loose',
+//       scissors: 'win',
+//       lizard:'loose',
+//       spock:'win',
+//     },
+//     paper: {
+//       rock: 'win',
+//       paper: 'draw',
+//       scissors: 'loose',
+//       lizard:'loose',
+//       spock:'win',
+//     },
+//     scissors: {
+//       rock: 'loose',
+//       paper: 'win',
+//       scissors: 'draw',
+//       lizard:'win',
+//       spock:'loose',
+//     },
+//     lizard: {
+//       rock: 'loose',
+//       paper: 'win',
+//       scissors: 'loose',
+//       lizard:'draw',
+//       spock:'win',
+//     },
+//     spock: {
+//       rock: 'loose',
+//       paper: 'loose',
+//       scissors: 'win',
+//       lizard:'loose',
+//       spock:'draw',
+//     },
+//   }
+//   const userChoice = 'scissors';
+//   const computerChoice = 'paper'
+//   const result = matrix[userChoice][computerChoice]
