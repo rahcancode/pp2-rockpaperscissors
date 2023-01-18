fb8ec356fe07ca39b5c92461648001efc59a1200
Sarah A <sarahmoore26@gmail.com>
2023-01-18 22:09:56 +0000

    Add media queries to css

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,34 +9,28 @@ const messages = document.getElementById("messages");
 const buttons = document.getElementsByClassName("controls");
 const choices = ["rock", "paper", "scissors"];
 
-// Added so function doesn't start until DOM Loads
-// Added click event listeners to buttons
+// Function for the HTML modals
 
-// Get the modal
 var modal = document.getElementById("myModal");
-
-// Get the button that opens the modal
 var btn = document.getElementById("myBtn");
-
-// Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
 
-// When the user clicks on the button, open the modal
 btn.onclick = function () {
     modal.style.display = "block";
 }
 
-// When the user clicks on <span> (x), close the modal
 span.onclick = function () {
     modal.style.display = "none";
 }
 
-// When the user clicks anywhere outside of the modal, close it
 window.onclick = function (event) {
     if (event.target == modal) {
         modal.style.display = "none";
     }
 }
+
+// Added so function doesn't start until DOM Loads
+// Added click event listeners to buttons
 
 document.addEventListener("DOMContentLoaded", function () {
 
@@ -160,7 +154,7 @@ function handleLeaderboardSubmit() {
 }
 
 function validateform(){  
-    var username = document.getElementById("leaderboard-input").username.value;
+    var username = document.get("leaderboard-input").username.value;
     if (username==null || username==""){  
       alert("Name can't be blank");  
       return false;
